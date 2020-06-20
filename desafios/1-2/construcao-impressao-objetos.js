a894ac0728866b7b1b6437f744d50f8c6a0b0c47
Luciana Nunes <lcnunes09@gmail.com>
2020-06-19 21:38:14 -0300

    Criando objeto da empresa Rockseat

---
+++ desafios/1-2/construcao-impressao-objetos.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nome: "Rockseat",
+    cor: "Roxo",
+    foco: "Programacao",
+    endereco: {
+        rua: "Rua Guilherme Gembala",
+        numero: 260
+    }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}.`);
