aded7795db891a3160c6c25d32d773d778fa29b4
Luciana Nunes <lcnunes09@gmail.com>
2020-06-20 15:04:33 -0300

    Renaming the file and refactoring to change language to english

---
+++ desafios/1-2/create-and-print-object.js
@@ -0,0 +1,11 @@
+const company = {
+    name: "Rockseat",
+    color: "Roxo",
+    foco: "Programacao",
+    address: {
+        street: "Rua Guilherme Gembala",
+        number: 260
+    }
+}
+
+console.log(`The company ${company.name} is currently at the location: ${company.address.street}, ${company.address.number}.`);
