ea2bc5212fec42a3d9969411898ff159c04948c8
Luciana Nunes <lcnunes09@gmail.com>
2020-06-20 14:56:52 -0300

    Renamming the file, refactoring document format, changing to english and for estructure

---
+++ desafios/1-3/user-technology.js
@@ -0,0 +1,51 @@
+const users = [
+    {
+        name: "Carlos",
+        technologies: ["HTML", "CSS"]
+    },
+    {
+        name: "Jasmine",
+        technologies: ["JavaScript", "CSS"]
+    },
+    {
+        name: "Tuane",
+        technologies: ["HTML", "Node.js"]
+    }
+]
+
+function checkIfUserIsCSSProgrammer(technologies) {
+    for (let technology of technologies) {
+        if (technology === 'CSS') {
+            return true
+        }
+    }
+
+    return false
+}
+
+function userTechStack(users) {
+    for (let user of users) {
+        messageUserTechStack(user)
+    }
+}
+
+function userCSSProgrammer(users) {
+    for (let user of users) {
+        userIsCSSProgrammer = checkIfUserIsCSSProgrammer(user.technologies)
+        if (userIsCSSProgrammer) {
+            messageUserIsCSSProgrammer(user)
+        }
+    }
+}
+
+function messageUserTechStack(user) {
+    console.log(`${user.name}'s Tech Stack: ${user.technologies}`)
+}
+
+function messageUserIsCSSProgrammer(user) {
+    console.log(`User ${user.name} develop using CSS`)
+}
+
+userTechStack(users)
+console.log("===============================")
+userCSSProgrammer(users)
